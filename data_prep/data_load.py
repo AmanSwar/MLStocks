a84fa757e3ae0e0d789143f62ebc7f25d383e9e5
Aman <aman.swar001@gmail.com>
2025-10-13 22:33:26 +0530

    feat : add function -> convert dataframe to tensor

--- data_prep/data_load.py
+++ data_prep/data_load.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import torch
 
 from data_prep.basic_feat import add_daily_return , indexify_date
 from data_prep.features import add_all_indicators
@@ -10,5 +11,11 @@ def prepare_data(path : str) -> pd.DataFrame:
   df = add_daily_return(df)
 
   return add_all_indicators(df)
+
+
+def dataframe_to_tensor(df : pd.DataFrame) -> torch.Tensor:
+  x_np = df.to_numpy()
+  return torch.from_numpy(x_np)
+
 
 
